56494e1d8954dd565f381d644ede6fa9335296f0
Ulric Wilfred <shaman.sir@gmail.com>
2015-04-03 01:09:18 +0200

    first data draft

---
+++ script/data.js
@@ -0,0 +1,70 @@
+var dataset = [
+    {
+        company: 'Animatron',
+        // where: 'Munich', 'Odessa', 'Boston'
+        //what: { 'gwt': 0.3, 'javascript': 0.7 }, // canvas
+        start: { 'month': 6, 'year': 2011 },
+        end: Infinity,
+        site: 'http://animatron.com',
+        project: 'http://animatron.com',
+        logo: ''
+
+    }, {
+
+        company: 'iPark Ventures',
+        //where: 'Odessa'
+        //what: { 'java'... }
+        start: { month: 4, year: 2010 },
+        end: { month: 6, year: 2011 },
+        site: '',
+        project: '',
+        logo: ''
+
+    }, {
+
+        company: 'Exectum LLC',
+        //where: 'Saint-Peterburg',
+        //what: { 'java'... }
+        start: { month: 9, year: 2009 },
+        end: { month: 2, year: 2010 },
+        site: '',
+        project: '',
+        logo: ''
+
+    }, {
+
+        company: '[Freelance]',
+        start: { month: 5, year: 2009 },
+        end: { month: 9, year: 2009 }
+
+    }, {
+
+        company: 'Sea Project',
+        start: { month: 8, year: 2009 },
+        end: { month: 8, year: 2009 }
+
+    }, {
+
+        company: 'Piclinq Rus / Fotonation Inc.',
+        start: { month: 7, year: 2007 },
+        end: { month: 5, year: 2009 }
+
+    }, {
+
+        company: 'FK Ltd.',
+        start: { month: 11, year: 2006 },
+        end: { month: 6, year: 2007 }
+
+    }, {
+
+        company: 'EmDev LLC',
+        start: { month: 5, year: 2005 },
+        end: { month: 10, year: 2006 }
+
+    }, {
+
+        company: 'VNIIOkeangeologiya',
+        start: { month: 4, year: 2004 },
+        end: { month: 3, year: 2005 }
+
+    } ];
