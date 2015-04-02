09811f3c4a595760a6dd1ff0ae92c87166c387ed
Ulric Wilfred <shaman.sir@gmail.com>
2015-04-03 01:08:58 +0200

    index text draft

--- script/main.js
+++ script/main.js
@@ -1,7 +1,5 @@
-var dataset = [ 5, 10, 15, 20, 26 ];
-
-d3.select("body").selectAll("p")
-    .data(dataset)
-    .enter()
-    .append("p")
-    .text("New paragraph!");
+// d3.select("body").selectAll("svg")
+//     .data(dataset)
+//     .enter()
+//     .append("p")
+//     .text("New paragraph!");
